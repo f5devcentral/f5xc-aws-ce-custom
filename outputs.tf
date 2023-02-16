2ff5f3eab413c5e52bcb4471b9333922eeec3fae
Andy Conley <a.conley@f5.com>
2023-02-15 21:19:43 -0700

    Updated Outputs

--- outputs.tf
+++ outputs.tf
@@ -2,11 +2,6 @@ output "f5xc_ce_az1_eip" {
   value = aws_eip.f5xc_ce_az1_outside.public_ip
 }
 
-output "f5xc_ce_az1_user_data" {
-  value = aws_instance.f5xc_ce_az1.user_data
-  sensitive = true
-}
-
 output "f5xc_ce_az2_eip" {
   value = aws_eip.f5xc_ce_az2_outside.public_ip
 }
