b2b05e1e87eb3a246b852f2efdbd239aa8bd82c6
Andy Conley <a.conley@f5.com>
2023-02-15 21:16:56 -0700

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,219 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+# Create Random ID to Append to Builds to Avoid Naming Conflicts
+resource "random_id" "buildSuffix" {
+  byte_length = 2
+}
+
+# Create Network Interfaces for Customer Edges
+resource "aws_network_interface" "f5xc_ce_az1_inside" {
+  subnet_id                 = var.az1_inside_subnet_id
+  private_ips_count         = 1
+  security_groups           = [var.inside_security_group]
+  source_dest_check         = false
+  private_ip_list_enabled   = false
+  ipv6_address_list_enabled = false
+  tags = {
+    Name  = "${var.project_prefix}-f5xc_ce_az1_inside-${random_id.buildSuffix.hex}"
+    Owner = var.resourceOwner
+  }
+}
+
+resource "aws_network_interface" "f5xc_ce_az1_outside" {
+  subnet_id                 = var.az1_outside_subnet_id
+  private_ips_count         = 1
+  security_groups           = [var.outside_security_group]
+  source_dest_check         = false
+  private_ip_list_enabled   = false
+  ipv6_address_list_enabled = false
+  tags = {
+    Name  = "${var.project_prefix}-f5xc_ce_az1_outside-${random_id.buildSuffix.hex}"
+    Owner = var.resourceOwner
+  }
+}
+
+resource "aws_eip" "f5xc_ce_az1_outside" {
+  vpc                       = true
+  network_interface         = aws_network_interface.f5xc_ce_az1_outside.id
+  associate_with_private_ip = aws_network_interface.f5xc_ce_az1_outside.private_ip
+  tags = {
+    Name  = "${var.project_prefix}-f5xc_ce_az1_outside_eipd-${random_id.buildSuffix.hex}"
+    Owner = var.resourceOwner
+  }
+}
+
+resource "aws_network_interface" "f5xc_ce_az2_inside" {
+  subnet_id                 = var.az2_inside_subnet_id
+  private_ips_count         = 1
+  security_groups           = [var.inside_security_group]
+  source_dest_check         = false
+  private_ip_list_enabled   = false
+  ipv6_address_list_enabled = false
+  tags = {
+    Name  = "${var.project_prefix}-f5xc_ce_az2_inside-${random_id.buildSuffix.hex}"
+    Owner = var.resourceOwner
+  }
+}
+
+resource "aws_network_interface" "f5xc_ce_az2_outside" {
+  subnet_id                 = var.az2_outside_subnet_id
+  private_ips_count         = 1
+  security_groups           = [var.outside_security_group]
+  source_dest_check         = false
+  private_ip_list_enabled   = false
+  ipv6_address_list_enabled = false
+  tags = {
+    Name  = "${var.project_prefix}-f5xc_ce_az2_outside-${random_id.buildSuffix.hex}"
+    Owner = var.resourceOwner
+  }
+}
+
+resource "aws_eip" "f5xc_ce_az2_outside" {
+  vpc                       = true
+  network_interface         = aws_network_interface.f5xc_ce_az2_outside.id
+  associate_with_private_ip = aws_network_interface.f5xc_ce_az2_outside.private_ip
+  tags = {
+    Name  = "${var.project_prefix}-f5xc_ce_az2_outside_eipd-${random_id.buildSuffix.hex}"
+    Owner = var.resourceOwner
+  }
+}
+
+resource "aws_network_interface" "f5xc_ce_az3_inside" {
+  subnet_id                 = var.az3_inside_subnet_id
+  private_ips_count         = 1
+  security_groups           = [var.inside_security_group]
+  source_dest_check         = false
+  private_ip_list_enabled   = false
+  ipv6_address_list_enabled = false
+  tags = {
+    Name  = "${var.project_prefix}-f5xc_ce_az3_inside-${random_id.buildSuffix.hex}"
+    Owner = var.resourceOwner
+  }
+}
+
+resource "aws_network_interface" "f5xc_ce_az3_outside" {
+  subnet_id                 = var.az3_outside_subnet_id
+  private_ips_count         = 1
+  security_groups           = [var.outside_security_group]
+  source_dest_check         = false
+  private_ip_list_enabled   = false
+  ipv6_address_list_enabled = false
+  tags = {
+    Name  = "${var.project_prefix}-f5xc_ce_az3_outside-${random_id.buildSuffix.hex}"
+    Owner = var.resourceOwner
+  }
+}
+
+resource "aws_eip" "f5xc_ce_az3_outside" {
+  vpc                       = true
+  network_interface         = aws_network_interface.f5xc_ce_az3_outside.id
+  associate_with_private_ip = aws_network_interface.f5xc_ce_az3_outside.private_ip
+  tags = {
+    Name  = "${var.project_prefix}-f5xc_ce_az3_outside_eipd-${random_id.buildSuffix.hex}"
+    Owner = var.resourceOwner
+  }
+}
+
+resource "aws_instance" "f5xc_ce_az1" {
+  ami           = var.amis[var.aws_region]
+  instance_type = var.instance_type
+  root_block_device {
+    volume_size = var.instance_disk_size
+    volume_type = "gp3"
+  }
+  get_password_data = false
+  monitoring        = false
+  availability_zone = var.az1
+
+  user_data_replace_on_change = true
+  user_data = templatefile("${path.module}/cloud_init.yaml.template",
+    {
+      sitetoken     = "${var.sitetoken}",
+      clustername   = "${var.clustername}",
+      sitelatitude  = "${var.sitelatitude}",
+      sitelongitude = "${var.sitelongitude}"
+    }
+  )
+
+  network_interface {
+    network_interface_id = aws_network_interface.f5xc_ce_az1_outside.id
+    device_index         = 0
+  }
+  network_interface {
+    network_interface_id = aws_network_interface.f5xc_ce_az1_inside.id
+    device_index         = 1
+  }
+  tags = {
+    Name = "${var.project_prefix}-master-0"
+  }
+}
+
+resource "aws_instance" "f5xc_ce_az2" {
+  ami           = var.amis[var.aws_region]
+  instance_type = var.instance_type
+  root_block_device {
+    volume_size = var.instance_disk_size
+    volume_type = "gp3"
+  }
+  get_password_data = false
+  monitoring        = false
+  availability_zone = var.az2
+
+  user_data_replace_on_change = true
+  user_data = templatefile("${path.module}/cloud_init.yaml.template",
+    {
+      sitetoken     = "${var.sitetoken}",
+      clustername   = "${var.clustername}",
+      sitelatitude  = "${var.sitelatitude}",
+      sitelongitude = "${var.sitelongitude}"
+    }
+  )
+
+  network_interface {
+    network_interface_id = aws_network_interface.f5xc_ce_az2_outside.id
+    device_index         = 0
+  }
+  network_interface {
+    network_interface_id = aws_network_interface.f5xc_ce_az2_inside.id
+    device_index         = 1
+  }
+  tags = {
+    Name = "${var.project_prefix}-master-1"
+  }
+}
+
+resource "aws_instance" "f5xc_ce_az3" {
+  ami           = var.amis[var.aws_region]
+  instance_type = var.instance_type
+  root_block_device {
+    volume_size = var.instance_disk_size
+    volume_type = "gp3"
+  }
+  get_password_data = false
+  monitoring        = false
+  availability_zone = var.az3
+
+  user_data_replace_on_change = true
+  user_data = templatefile("${path.module}/cloud_init.yaml.template",
+    {
+      sitetoken     = "${var.sitetoken}",
+      clustername   = "${var.clustername}",
+      sitelatitude  = "${var.sitelatitude}",
+      sitelongitude = "${var.sitelongitude}"
+    }
+  )
+
+  network_interface {
+    network_interface_id = aws_network_interface.f5xc_ce_az3_outside.id
+    device_index         = 0
+  }
+  network_interface {
+    network_interface_id = aws_network_interface.f5xc_ce_az3_inside.id
+    device_index         = 1
+  }
+  tags = {
+    Name = "${var.project_prefix}-master-2"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,122 @@
+variable "aws_region" {
+  type = string
+}
+
+variable "az1" {
+  type = string
+}
+
+variable "az2" {
+  type = string
+}
+
+variable "az3" {
+  type = string
+}
+
+variable "project_prefix" {
+  type    = string
+  default = "demo"
+}
+
+variable "resourceOwner" {
+  # used for "owner" tags in AWS
+  type        = string
+  description = "Owner of the deployment for tagging purposes"
+  default     = "demo.user"
+}
+
+variable "az1_outside_subnet_id" {
+  type        = string
+  description = "The subnet ID for the outside subnet in Availability Zone 1"
+}
+
+variable "az1_inside_subnet_id" {
+  type        = string
+  description = "The subnet ID for the inside subnet in Availability Zone 1"
+}
+
+
+variable "az2_outside_subnet_id" {
+  type        = string
+  description = "The subnet ID for the outside subnet in Availability Zone 2"
+}
+
+variable "az2_inside_subnet_id" {
+  type        = string
+  description = "The subnet ID for the inside subnet in Availability Zone 2"
+}
+
+variable "az3_outside_subnet_id" {
+  type        = string
+  description = "The subnet ID for the outside subnet in Availability Zone 3"
+}
+
+variable "az3_inside_subnet_id" {
+  type        = string
+  description = "The subnet ID for the inside subnet in Availability Zone 3"
+}
+
+variable "outside_security_group" {
+    type = string
+    description = "The Security Group ID for the outside interfaces"
+}
+
+variable "inside_security_group" {
+    type = string
+    description = "The Security Group ID for the inside interfaces"
+}
+
+
+variable "amis" {
+  type = map(any)
+  default = {
+    "ca-central-1"   = "ami-052252c245ff77338"
+    "af-south-1"     = "ami-0c22728f79f714ed1"
+    "ap-east-1"      = "ami-0a6cf3665c0612f91"
+    "ap-northeast-2" = "ami-01472d819351faf92"
+    "ap-southeast-2" = "ami-03ff18dfb7f90eb54"
+    "ap-south-1"     = "ami-0277ab0b4db359c93"
+    "ap-northeast-1" = "ami-0384d075a36447e2a"
+    "ap-southeast-1" = "ami-0d6463ee1e3727e84"
+    "eu-central-1"   = "ami-06d5e0073d97ecf99"
+    "eu-west-1"      = "ami-090680f491ad6d46a"
+    "eu-west-3"      = "ami-03bd7c41ca1b586a8"
+    "eu-south-1"     = "ami-0baafa10ffcd081b7"
+    "eu-north-1"     = "ami-006c465449ed98c69"
+    "eu-west-2"      = "ami-0df8a483722043a41"
+    "me-south-1"     = "ami-094efc1a78169dd7c"
+    "sa-east-1"      = "ami-07369c4b06cf22299"
+    "us-east-1"      = "ami-089311edbe1137720"
+    "us-east-2"      = "ami-01ba94b5a83adcb35"
+    "us-west-1"      = "ami-092a2a07d2d3a445f"
+    "us-west-2"      = "ami-07252e5ab4023b8cf"
+  }
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t3.xlarge"
+}
+
+variable "instance_disk_size" {
+  type    = string
+  default = "40"
+}
+variable "sitelatitude" {
+  type        = string
+  description = "REQUIRED: Site Physical Location Latitude. See https://www.latlong.net/"
+}
+variable "sitelongitude" {
+  type        = string
+  description = "REQUIRED: Site Physical Location Longitude. See https://www.latlong.net/"
+}
+variable "clustername" {
+  type        = string
+  description = "REQUIRED: Site Cluster Name."
+}
+variable "sitetoken" {
+  type        = string
+  sensitive   = true
+  description = "REQUIRED: Site Registration Token."
+}
